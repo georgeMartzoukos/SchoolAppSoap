5bc26efd1be3cbaf55251976d5bc11214de960a4
Georgios Martzoukos <georgemartzoukos1@gmail.com>
2023-03-12 14:49:30 +0200

    Update DBUtil.java

--- src/main/java/gr/aueb/cf/schoolapp/service/util/DBUtil.java
+++ src/main/java/gr/aueb/cf/schoolapp/service/util/DBUtil.java
@@ -14,7 +14,6 @@ public class DBUtil {
     static {
         ds.setUrl("jdbc:mysql://localhost:3306/schooldb?serverTimeZone=UTC");
         ds.setUsername("user1");
-        //ds.setPassword("user1");
         ds.setPassword(System.getenv("TS_USER_PASSWORD"));
         ds.setInitialSize(8);
         ds.setMaxTotal(32);
